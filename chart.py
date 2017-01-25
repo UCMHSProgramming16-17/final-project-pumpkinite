ee63bce7f865b3263d2cb8e1d71da4b6806858c4
Nika Zaslavsky <bzaslavsky@ucvts.org>
2017-01-25 23:27:04 +0000

    worked on chart

--- chart.py
+++ chart.py
@@ -3,66 +3,71 @@ import pandas as pd
 
 from bokeh.plotting import figure, show
 from bokeh.models import HoverTool, ColumnDataSource
-from bokeh.sampledata.les_mis import data
 
+# import data
 df = pd.read_csv('prescriber-info.csv')
 
-nodes = data['nodes']
-names = [node['name'] for node in sorted(data['nodes'], key=lambda x: x['group'])]
+### organize data for axes and counts
 
-N = len(nodes)
-counts = np.zeros((N, N))
-for link in data['links']:
-    counts[link['source'], link['target']] = link['value']
-    counts[link['target'], link['source']] = link['value']
+# create sorted lists containing all fields of medicine and drugs
+specialties = sorted(set([name for name in df["Specialty"]]))
+drugs = [name for name in df.columns.values]
+# clean drugs list
+del drugs[0:5]
+del drugs[-1]
 
-colormap = ["#444444", "#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99",
-            "#e31a1c", "#fdbf6f", "#ff7f00", "#cab2d6", "#6a3d9a"]
+# create empty matrix to store counts in
+counts = np.zeros((len(specialties), len(drugs)))
+# iterate through specialties
+for specialty in specialties:
+    # select only doctors with the specified specialty
+    specdf = df.loc[df["Specialty"] == specialty]
+    # iterate through drugs
+    for drug in drugs:
+        # sum all prescriptions for specified drug and assign count value
+        counts[specialties.index(specialty), drugs.index(drug)] = specdf[drug].sum()
+        
+### set up coloring scheme
 
-xname = []
-yname = []
-color = []
-alpha = []
-for i, node1 in enumerate(nodes):
-    for j, node2 in enumerate(nodes):
-        xname.append(node1['name'])
-        yname.append(node2['name'])
 
-        alpha.append(min(counts[i,j]/4.0, 0.9) + 0.1)
+### visualization
 
-        if node1['group'] == node2['group']:
-            color.append(colormap[node1['group']])
-        else:
-            color.append('lightgrey')
-
+# organize the data
 source = ColumnDataSource(data=dict(
-    xname=xname,
-    yname=yname,
-    colors=color,
-    alphas=alpha,
+    x=drugs,
+    y=specialties,
+    #colors=color,
+    #alphas=alpha,
     count=counts.flatten(),
 ))
 
-p = figure(title="Les Mis Occurrences",
-           x_axis_location="above", tools="hover,save",
-           x_range=list(reversed(names)), y_range=names)
+# toolbar
+TOOLS = "pan,wheel_zoom,box_zoom,reset,hover,save"
 
-p.plot_width = 800
+# format chart
+p = figure(title="2014 Medicare Drug Prescriptions by Specialty",
+           x_axis_location="above", tools=TOOLS,
+           x_range=drugs, y_range=specialties)
+p.plot_width = 1600
 p.plot_height = 800
 p.grid.grid_line_color = None
 p.axis.axis_line_color = None
 p.axis.major_tick_line_color = None
-p.axis.major_label_text_font_size = "5pt"
+p.axis.major_label_text_font_size = "4pt"
 p.axis.major_label_standoff = 0
 p.xaxis.major_label_orientation = np.pi/3
 
-p.rect('xname', 'yname', 0.9, 0.9, source=source,
-       color='colors', alpha='alphas', line_color=None,
-       hover_line_color='black', hover_color='colors')
+# draw squares
+p.rect('x', 'y', 1.5, 1.5, source=source,
+       #color='colors', hover_color='colors', alpha='alphas'
+       line_color=None, hover_line_color='black')
 
+# set up hovering tooltips
 p.select_one(HoverTool).tooltips = [
-    ('names', '@yname, @xname'),
-    ('count', '@count'),
+    ('Specialty', '@y'),
+    ('Drug', '@x'),
+    ('Number of Prescriptions', '@count'),
 ]
 
+# show chart
 show(p)
